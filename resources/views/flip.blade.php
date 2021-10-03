7d4aa94b62d9bd98224ddcd6a65a673c7720a835
munawir4 <munawir4@gmail.com>
2021-10-03 10:34:46 +0530

    new

---
+++ resources/views/flip.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body align="center">
+    <div class="ll">
+        <h1 class="color">
+            <u>WELCOME TO FLIPKART</u>
+        </h1>
+        <img src="fd.jpg" alt="">
+
+
+        <form action="" method="post">
+            <br>
+            <br>
+            <button><a href="http://127.0.0.1:8000/login">LOGIN</a></button>
+
+
+
+        </form>
+
+
+
+
+
+
+
+    </div>
+</body>
+
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body align="center" cellspacing="300">
+    <div class="div">
+        <form action="" method="post" cellspacing="100">
+
+            <input class="lp" type="email and number" name="" id="" placeholder="Enter Email/Mobile Number">
+            <br>
+            <br>
+            <input class="lp" type="password" name="" id="" placeholder="Enter Password" height="50">
+            <br>
+
+            <br>
+            <div class="ht"> By continuing, You Agree to Flipkart's terms of use and Privacy Policy.
+                <br><br>
+                <button class="gp">Login</button>
+                <br> OR
+                <br>
+                <button class="gp kk">Request OTP</button>
+                <br><br><br>
+                <a href="">Create New Account </a>
+                <br>
+                <br>
+                <a href="http://127.0.0.1:8000/fp">BACK</a>
+
+
+        </form>
+
+
+        </div>
+</body>
+
+</html>
